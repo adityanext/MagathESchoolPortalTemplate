85be85bd141edbce923d25c863e98ad62e470724
ashrivastavaboutiqaat <a.shrivastva@boutiqaat.com>
2018-08-15 08:31:51 +0530

    Added navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import{ HeaderComponent } from './header/header.component';
+import{ RightSidebarComponent } from './right-sidebar/right-sidebar.component';
+import{ LoginComponent } from './login/login.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {
+    path:'',
+    component:RightSidebarComponent
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
+import { RightSidebarComponent } from './right-sidebar/right-sidebar.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    LeftSidebarComponent,
+    RightSidebarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
